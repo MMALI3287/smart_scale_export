259da9b9426d61416900eda4ad7baa345151dd23
MMALI3287 <erfanali3287@gmail.com>
2024-03-29 15:24:40 +0600

    modified graphs

--- lib/weekly_tabs.dart
+++ lib/weekly_tabs.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
